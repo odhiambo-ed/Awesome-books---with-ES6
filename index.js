b6bf68af78bd47db55b43d7ed39f2a000339dd47
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-22 10:47:32 +0300

    Add Modules

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import { date } from './modules/date.js';
-// import bookList from './modules/bookList.js';
-// import { addBook } from './modules/addBook.js';
-// import { removeBook } from './modules/removeBook.js';
+import { recordNewBook } from "../modules/recordNewBook.mjs";
+import { deleteBookRecord } from "../modules/deleteBookRecord.mjs";
 
-date();
-// bookList = new BookList();
-// addBook();
-// removeBook();
+import { BookList } from "../modules/bookList.mjs";
+
+import { showTime } from "../modules/displayTime.mjs";
+
+showTime()
+
+const bookList = new BookList();
+
+// Submit Event Listener
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+    recordNewBook(bookList, e);
+});
+
+// Event listener when the user clicks on remove button
+document.addEventListener("click", (e) => {
+    deleteBookRecord(bookList, e);
+});
---
+++ modules/deleteBookRecord.mjs
@@ -0,0 +1,14 @@
+export const deleteBookRecord = (bookList, e) => {
+    const deleteButton = e.target;
+    if (
+        deleteButton !== undefined &&
+        deleteButton.classList.contains("delete")
+    ) {
+        e.preventDefault();
+        const tr = deleteButton.closest("tr");
+        const isbn = tr.getAttribute("data-isbn");
+        tr.remove();
+
+        bookList.removeNewBook(isbn);
+    }
+}
